28cd4e346a799a305aca80289005d58b4f06f14f
San-43 <virgosmp.mx@gmail.com>
2022-03-01 23:58:23 -0600

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import Router from 'next/router';
 import Head from 'next/head';
-import NProgress  from 'nprogress';
+import NProgress from 'nprogress';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import Layout from '../components/Layout.jsx';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  NProgress.configure({ showSpinner: false })
+  NProgress.configure({ showSpinner: false });
 
   Router.events.on('routeChangeStart', () => {
     NProgress.start();
