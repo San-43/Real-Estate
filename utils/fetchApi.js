6bbc2e5195a97dec0d79f8ab198663ac7284278a
San-43 <virgosmp.mx@gmail.com>
2022-03-02 00:02:26 -0600

    fix

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
-
-    const { data } = await axios.get((url), {
-        headers: {
-            'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'f739dd9e95msh53ac9d2e9b9597ap10d439jsnf74f0d3291e8'
-        }
-    });
-
-    return data;
+  const { data } = await axios.get((url), {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+    },
+  });
+    
+  return data;
 }
